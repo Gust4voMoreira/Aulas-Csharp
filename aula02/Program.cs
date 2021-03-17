db2d3d6d36e292aaf3040aefe130ee97bd943c95
Gustavo Moreira <gm1230133@gmail.com>
2021-03-16 20:27:25 -0400

    aulas

---
+++ aula02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace aula02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.Write("Digite seu Salário: ");
+            var salario = double.Parse(Console.ReadLine());
+            var p = salario * 0.25;
+            Console.Write("aaaaa"+p);
+        }
+    }
+}
