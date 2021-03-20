4a6ea467949fda406fafb4f99a7485027afa2488
Gustavo Moreira <gm1230133@gmail.com>
2021-03-20 15:11:31 -0400

    aulas

---
+++ aulaa/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace aulaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x, y;
+            Console.Write("Digite o primeiro número: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            y = int.Parse(Console.ReadLine());
+
+            if (x == y)
+            {
+                //Console.Write("o número x: "+x+" e o y: "+y+" são iguais");
+                Console.Write($"o número x:{x} e o y:{y} são iguais");
+            }
+            else if (x > y)
+            {
+                //Console.Write("O maior número é x: " + x);
+                Console.Write($"O número {x} é o maoir");
+            }
+            else
+            {
+                //Console.Write("O maior número é y: " + y);
+                Console.Write($"O número {y} é o maoir");
+            }
+            */
+
+            Console.WriteLine("Introduza x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduza y: ");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduza y: ");
+            int z = int.Parse(Console.ReadLine());
+           
+            if(x > y){
+                if(x > z){
+                    Console.WriteLine("O maior é x: "+x);
+                } else {
+                    Console.WriteLine("O maior é z: "+z);
+                }
+            } else {
+                if(y > z){
+                    Console.WriteLine("O maior é y: "+y);
+                } else {
+                    Console.WriteLine("O maior é z: "+z);
+                }
+            }
+
+            //>se x > y
+            //se x > z = x
+            //se não z
+            //
+            //>se não
+            //se y > z = y
+            //se não z
+        }
+    }
+}
