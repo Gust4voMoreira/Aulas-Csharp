599a2fe61daf3b1f1ea14dd8a1e8f0e69aca5632
Gustavo Moreira <gm1230133@gmail.com>
2021-03-30 21:52:24 -0400

    aula07

--- aula07/Program.cs
+++ aula07/Program.cs
@@ -17,17 +17,15 @@ namespace aula07
             Console.Write("valor C: ");
             c = float.Parse(Console.ReadLine());
 
-            if((a + b) > c){
-                Console.Write($" A + B = {a+b}, é maior que c");
-            } else {
-                Console.Write($"C = {c}, é maior que a soma de A e B");
+            if ((a + b) > c)
+            {
+                Console.Write($"A soma de A + B é maior que o C");
             }
             */
 
             /*
             2) Faça um algoritmo que leia o nome, o sexo e o estado civil de uma pessoa. Caso sexo seja “F” e estado civil seja “CASADA”, solicitar o ano que casou e mostrar o tempo de casada.
-            */
-
+            
             string nome, estado_civil;
             char sexo;
             int tempo_casada, ano_casou, ano = DateTime.Now.Year;
@@ -39,7 +37,8 @@ namespace aula07
             Console.WriteLine("Estado civil: ");
             estado_civil = Console.ReadLine();
 
-            if(sexo == 'F' && estado_civil == "CASADA"){
+            if (sexo == 'F' && estado_civil == "CASADA")
+            {
                 Console.WriteLine("Digite o ano em que casou: ");
                 ano_casou = int.Parse(Console.ReadLine());
 
@@ -47,10 +46,26 @@ namespace aula07
 
                 Console.WriteLine($"Tempo de casada é {tempo_casada} anos");
             }
+            */
 
             /*
             3) Faça um algoritmo que o leia um número e informar se o número é par ou ímpar.
             */
+
+            //erro
+            int n1, resto = n1 % 2;
+
+            Console.Write("Digite um número: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            if (resto == 0)
+            {
+                Console.WriteLine($"O número {n1} é par");
+            }
+            else
+            {
+                Console.WriteLine($"O número {n1} é impar");
+            }
         }
     }
 }
