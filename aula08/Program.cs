e46cd80c7d621beb37199186908e79d3d99896f0
Gustavo Moreira <gm1230133@gmail.com>
2021-04-11 15:15:03 -0400

    aula08

--- aula08/Program.cs
+++ aula08/Program.cs
@@ -6,7 +6,62 @@ namespace aula08
     {
         static void Main(string[] args)
         {
-            //Data: 06/04/2021
+            //Data: 10/04/2021
+            /*
+            1)Faça um algoritmo em C# que leia um número inteiro entre 1 e 12 e escrever o mês 
+            correspondente. Caso o usuário digite um número fora desse intervalo, deverá 
+            aparecer uma mensagem informando que não existe mês com este número.
+            
+            Console.Write("Digite um Número: ");
+            int mes = int.Parse(Console.ReadLine());
+
+            switch (mes)
+            {
+                case 1:
+                    Console.WriteLine("JANEIRO");
+                    break;
+                case 2:
+                    Console.WriteLine("FEVEREIRO");
+                    break;
+                case 3:
+                    Console.WriteLine("MARÇO");
+                    break;
+                case 4:
+                    Console.WriteLine("ABRIL");
+                    break;
+                case 5:
+                    Console.WriteLine("MAIO");
+                    break;
+                case 6:
+                    Console.WriteLine("JUNHO");
+                    break;
+                case 7:
+                    Console.WriteLine("JULHO");
+                    break;
+                case 8:
+                    Console.WriteLine("AGOSTO");
+                    break;
+                case 9:
+                    Console.WriteLine("SETEMBRO");
+                    break;
+                case 10:
+                    Console.WriteLine("OUTUBRO");
+                    break;
+                case 11:
+                    Console.WriteLine("NOVEMBRO");
+                    break;
+                case 12:
+                    Console.WriteLine("DEZEMBRO");
+                    break;
+                default:
+                    Console.WriteLine("Mês Inválido");
+                    break;  
+            }
+            */
+
+            /*
+            2)
+            */
         }
     }
 }
