59f02ef46087854a5d919cf5233c043c6c52dc0b
Gustavo Moreira <gm1230133@gmail.com>
2021-04-21 10:17:56 -0400

    aula09

--- aula09/Program.cs
+++ aula09/Program.cs
@@ -8,17 +8,17 @@ namespace aula09
         {
             //Data: 20/04/2021
             /*
-            1)
+            1) Faça um algoritmo para calcular a somatória dos número de 1 até 10;
             int num = 1, soma = 0;
 
             while (num <= 10)
             {
-                Console.WriteLine(num + soma);
-                soma += num;
-                num ++;
-
-            //55
+                // soma = soma + num; //acumulador
+                soma += num; //acumulador
+                //  num = num + 1; //incremento
+                num++; //incremento
             }
+            Console.WriteLine(soma);
             */
 
             /*
@@ -54,7 +54,11 @@ namespace aula09
             */
 
             /*
-            4)
+            4) Faça um algoritmo para somar todos os números inteiros compreendidos entre 2 
+            números, sendo que:
+            - Os dois números serão informados pelo usuário
+            - O número inicial não poderá ser maior ou igual ao número final
+
             int numIni, numFim,soma = 0;
 
             Console.Write("Digite o número inicial: ");
@@ -64,7 +68,7 @@ namespace aula09
 
             while (numIni >= numFim)
             {
-                Console.WriteLine("O némero inicial é menor que o número final");
+                Console.WriteLine("O número inicial tem que ser menor que o final!");
                 Console.WriteLine("Digite o número inicial: ");
                 numIni = int.Parse(Console.ReadLine());
                 Console.WriteLine("Digite o número final: ");
@@ -75,23 +79,46 @@ namespace aula09
                 soma += numIni;
                 numIni++;
             }
-            Console.WriteLine(soma);
+            Console.WriteLine($"A somatória entre os númeroes é {soma}");
             */
 
-            int num,quantidade,media,soma = 0;
+            /*
+            5) Desenvolver um algoritmo capaz de calcular a média aritmética simples de uma série 
+            de números, sendo que:
+            - O usuário deve informar a quantidade de números da série.
+            - Se a quantidade for zero ou negativa o algoritmo não deve aceita-la.
+            - O usuário deve informar um por um todos os números da série.
+            - O algoritmo deve mostrar como resultado a média aritmética simples calculada 
+            através da formula:
+            MEDIA = (X + X1 + X2+...XN) / N.
+            */
+            int qtdSerie, cont = 1;
+            float numero, soma = 0, media;
 
-            Console.Write("Digite o número inicial: ");
-            num = int.Parse(Console.ReadLine());
-            Console.Write("Digite o quantidade: ");
-            quantidade = int.Parse(Console.ReadLine());
+            Console.Write("Digite a quantidade de números que será informado: ");
+            qtdSerie = int.Parse(Console.ReadLine());
 
-            while (num >= quantidade && quantidade != 0)
+            while (qtdSerie <= 0)
             {
-                Console.WriteLine("Digite o número inicial: ");
-                num = int.Parse(Console.ReadLine());
-                soma += num;
-                num++;
+                Console.WriteLine("Quantidade inválida! Não pode ser negativo nem igual a zero");
+
+                Console.Write("Digite a quantidade de números que será informado: ");
+                qtdSerie = int.Parse(Console.ReadLine());
             }
+
+            while (cont <= qtdSerie)
+            {
+                Console.Write($"Digite o {cont}º número: ");
+                numero = float.Parse(Console.ReadLine());
+                soma += numero;
+
+                cont ++;
+            }
+
+            media = soma / qtdSerie;
+
+            Console.Write($"A média dos números é: {media}");
+
         }
     }
 }
