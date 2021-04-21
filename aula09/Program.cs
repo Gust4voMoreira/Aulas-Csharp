76eb82208b9fa0f454d838cf05fdc49cec29d59d
Gustavo Moreira <gm1230133@gmail.com>
2021-04-20 20:35:41 -0400

    aula09

--- aula09/Program.cs
+++ aula09/Program.cs
@@ -7,16 +7,47 @@ namespace aula09
         static void Main(string[] args)
         {
             //Data: 20/04/2021
-            
+            /*
+            1)
             int num = 1, soma = 0;
 
             while (num <= 10)
             {
                 Console.WriteLine(num + soma);
-                soma = num + soma;
+                soma += num;
                 num ++;
 
             //55
+            }
+            */
+
+            /*
+            2)
+            int numIni, numFim,soma = 0;
+
+            Console.Write("Digite o número inicial: ");
+            numIni = int.Parse(Console.ReadLine());
+            Console.Write("Digite o número final: ");
+            numFim = int.Parse(Console.ReadLine());
+
+            while (numIni <= numFim)
+            {
+                soma += numIni;
+                numIni++;
+            }
+            Console.WriteLine(soma);
+            */
+
+            char continuar = 's';
+            int cont = 1;
+
+            while(continuar == 's')
+            {
+                Console.WriteLine($"{cont}° volta no laço.");
+                cont ++;
+
+                Console.WriteLine($"Deseja repetir (s) ou (n)");
+                continuar = char.Parse(Console.ReadLine());
             }
         }
     }
