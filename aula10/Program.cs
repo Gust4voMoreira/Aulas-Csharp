eaf1a7410068c91413837e4dd85779b211b0df06
Gustavo Moreira <gm1230133@gmail.com>
2021-04-24 14:38:01 -0400

    aula10

--- aula10/Program.cs
+++ aula10/Program.cs
@@ -1,13 +1,44 @@
 ﻿using System;
 
-namespace aula10
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace aula10 {
+    class Program {
+        static void Main(string[] args) {
             //Data: 24/04/2021
-            
+
+            /*
+            //1)
+            int cont = 1;
+            while (cont <= 10)
+            {
+                Console.WriteLine($"{cont} Gustavo");
+                cont ++;
+            }
+            //--------------
+            //2)
+            int cont = 11;
+            do
+            {
+                Console.WriteLine($"{cont} Gustavo");
+                cont ++;
+            } 
+            while (cont <= 10);
+            //-------------
+            //3)
+            for (int i = 1; i <= 10; i--) 
+            {
+                Console.WriteLine($"{i} Gustavo");
+            }
+            */
+
+            /*
+            4) Escreva um programa que escreva os números ímpares entre 100 e 200
+            for (int i = 100; i <= 200; i++)
+            {
+                if (i % 2 != 0){
+                    Console.WriteLine(i);
+                }
+            }
+            */
         }
     }
 }
