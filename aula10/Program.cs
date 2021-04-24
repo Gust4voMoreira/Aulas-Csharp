73bc8288c6863531873757713d1d20ee41dbf4fb
Gustavo Moreira <gm1230133@gmail.com>
2021-04-24 13:53:40 -0400

    alteração

---
+++ aula10/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace aula10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Data: 24/04/2021
+            
+        }
+    }
+}
