19efe028ec16f9aa2938ab46f49e53bbaa32561d
Gustavo Moreira <gm1230133@gmail.com>
2021-05-03 10:44:25 -0300

    aula11

--- aula11/Program.cs
+++ aula11/Program.cs
@@ -13,27 +13,40 @@ namespace aula11
             O número para o cálculo deve ser informado pelo usuário
             O algoritmo deve informar o fatorial deste número de acordo com as regras
             abaixo:
-            • d fatorial de números negativos não existe
+            • o fatorial de números negativos não existe
             • O fatorial de 0 é 1
             • O fatorial de 1 é 1
-            */
-            int num, fatorial;
+    
+            int num, fat;
 
             Console.WriteLine("Digite um número: ");
             num = int.Parse(Console.ReadLine());
 
             if (num == 0 || num == 1)
             {
-                Console.Write($"Fatorial de {num} é 1");
+                Console.WriteLine($"Fatorial de {num} é 1");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine($"o fatorial de números negativos não existe.");
             }
             else
             {
-                for (int  i = 0;  i > num; i--)
+                // for (fat = 1; num > 1; num = num - 1)
+                // {
+                //     fat = fat * num;
+                // }
+                // Console.WriteLine(fat);
+                //ou
+                fat = 1;
+                while (num > 1)
                 {
-                    fatorial = num * i;
-                    Console.WriteLine($"Fatorial de {num} é {fatorial}");
+                    fat = fat * num; 
+                    num = num -1;
                 }
+                Console.WriteLine(fat);
             }
+            */
             
         }
     }
