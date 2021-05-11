bf2c3b5b2202ed25000c81aa9db32d3694530f1e
Gustavo Moreira <gm1230133@gmail.com>
2021-05-11 19:42:49 -0300

    aula13

--- aula13/Program.cs
+++ aula13/Program.cs
@@ -6,7 +6,22 @@ namespace aula13
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Data: 11/05/2021
+            //1) Ler números inteiros com vetor.
+            int[] vetNum = new int[10];
+
+            for (int i = 0; i < vetNum.Length; i++)
+            {
+                Console.WriteLine($"Digite um número na posição {i} do vetor:");
+                vetNum[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("O VETOR PREENCHIDO");
+
+            for (int i = 0; i < vetNum.Length; i++)
+            {
+                Console.Write($"{vetNum[i]}|");
+            }
         }
     }
 }
