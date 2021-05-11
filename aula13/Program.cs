160b4f5be1c949373e64c41cfc534f96507edb21
Gustavo Moreira <gm1230133@gmail.com>
2021-05-11 19:57:06 -0300

    aula13

--- aula13/Program.cs
+++ aula13/Program.cs
@@ -7,7 +7,7 @@ namespace aula13
         static void Main(string[] args)
         {
             //Data: 11/05/2021
-            //1) Ler números inteiros com vetor.
+            /*1) Ler números inteiros com vetor.
             int[] vetNum = new int[10];
 
             for (int i = 0; i < vetNum.Length; i++)
@@ -21,6 +21,26 @@ namespace aula13
             for (int i = 0; i < vetNum.Length; i++)
             {
                 Console.Write($"{vetNum[i]}|");
+            }
+            */
+
+            //2)
+            int[] vetIdade = new int[5];
+            string[] vetNome = new string[5];
+
+            for (int i = 0; i < vetNome.Length; i++)
+            {
+                Console.Write($"Digite o nome do {i + 1}° aluno: ");
+                vetNome[i] = Console.ReadLine();
+                Console.Write($"Digite a idade do {i + 1}° aluno: ");
+                vetIdade[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("O VETOR PREENCHIDO");
+
+            for (int i = 0; i < vetNome.Length; i++)
+            {
+                Console.Write($"{vetNome[i]} tem {vetIdade[i]} anos");
             }
         }
     }
