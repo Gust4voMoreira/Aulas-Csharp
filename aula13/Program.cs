aa7a49032dab4dcf91e727a3a1e47fbcd1a6e472
Gustavo Moreira <gm1230133@gmail.com>
2021-05-11 20:38:18 -0300

    aula13

--- aula13/Program.cs
+++ aula13/Program.cs
@@ -7,7 +7,8 @@ namespace aula13
         static void Main(string[] args)
         {
             //Data: 11/05/2021
-            /*1) Ler números inteiros com vetor.
+            /*1) Ler 10 números inteiros utilizando vetor.
+
             int[] vetNum = new int[10];
 
             for (int i = 0; i < vetNum.Length; i++)
@@ -24,7 +25,8 @@ namespace aula13
             }
             */
 
-            //2)
+            /*2) Faça um programa que o usuário informe o nome e a idade de cinco alunos. Utilize um vetor para o cada um dados informados, ou seja, um vetor para os nomes e um vetor para as idades. Após o preenchimento dos vetores mostrar os nomes com suas respectivas idades.
+
             int[] vetIdade = new int[5];
             string[] vetNome = new string[5];
 
@@ -42,6 +44,50 @@ namespace aula13
             {
                 Console.Write($"{vetNome[i]} tem {vetIdade[i]} anos");
             }
+            */
+
+            //Ex:
+            /*1) Crie um programa que leia 6 valores inteiros, armazene em um vetor, em seguida, 
+            mostre na tela os valores lidos.
+
+            int[] vetNum = new int[6];
+
+            for (int i = 0; i < vetNum.Length; i++)
+            {
+                Console.WriteLine($"Digite um número na posição {i} do vetor:");
+                vetNum[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("O VETOR PREENCHIDO");
+
+            for (int i = 0; i < vetNum.Length; i++)
+            {
+                Console.Write($"{vetNum[i]}|");
+            }
+            */
+
+            /*2)Faça um algoritmo que leia um conjunto de números inteiros, armazenando-o em 
+            vetor e calcular o quadrado das componentes deste vetor, armazenando o resultado em 
+            outro vetor. Cada vetor tem 10 elementos cada. Mostrar os valores dos dois vetores.
+
+            int[] vetNum = new int[10];
+            int[] vetQuad = new int[10];
+
+            for (int i = 0; i < vetNum.Length; i++)
+            {
+                Console.WriteLine($"Digite um número na posição {i + 1} do vetor:");
+                vetNum[i] = int.Parse(Console.ReadLine());
+
+                vetQuad[i] = vetNum[i] * vetNum[i];
+            }
+
+            Console.WriteLine("O QUADRADO DO VETOR PREENCHIDO");
+
+            for (int i = 0; i < vetNum.Length; i++)
+            {
+                Console.WriteLine($"O quadrado do número {vetNum[i]} é {vetQuad[i]}");
+            }
+            */
         }
     }
 }
