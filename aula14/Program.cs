2500d52dc007a27582631101f24523960e6cdd65
Gustavo Moreira <gm1230133@gmail.com>
2021-05-25 20:45:47 -0300

    aula14

--- aula14/Program.cs
+++ aula14/Program.cs
@@ -7,7 +7,18 @@ namespace aula14
         static void Main(string[] args)
         {
             //Data: 25/05/2021
-            
+            for (int i = 0; i <= 10; i++)
+            {
+                for (int u = 0; u < 11; u++)
+                {
+                    Console.WriteLine($"{i} x {u} = {Mult(i,2)}");
+                }
+            }
+        }
+
+        static int Mult(int x, int y)
+        {
+            return x * y;
         }
     }
 }
