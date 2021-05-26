4d3932ecac1131fc5e571427aeae334d4ee29f74
Gustavo Moreira <gm1230133@gmail.com>
2021-05-25 22:39:47 -0300

    alteração

--- aula14.1/Program.cs
+++ aula14.1/Program.cs
@@ -7,13 +7,23 @@ namespace aula14._1
         static void Main(string[] args)
         {
             //Data:25/05/2021
-            int qtdAlunos;
+            int qtd;
 
             Console.WriteLine("Calcular Média Bimestral");
             Console.WriteLine("Digite o número de alunos da turma:");
-            qtdAlunos = int.Parse(Console.ReadLine());
+            qtd = int.Parse(Console.ReadLine());
 
-            var ntPrim = pVetor(qtdAlunos);
+            Console.WriteLine("Primeiro Bimestre");
+            var notPri = pVetor(qtd);
+            Console.WriteLine("Segundo Bimestre");
+            var notSeg = pVetor(qtd);
+
+            Console.WriteLine("Notas Primeiro Bimestre");
+            imprimir(notPri);
+            Console.WriteLine("Notas Segundo Bimestre");
+            imprimir(notSeg);
+            impMedia(notPri, notSeg);
+            impResul(notPri, notSeg);
         }
 
         static float[] pVetor(int qtd)
@@ -22,10 +32,48 @@ namespace aula14._1
 
             for (int i = 0; i < vetNotas.Length; i++)
             {
-                Console.WriteLine($"Digite a nota do {i+1}° aluno: ");
+                Console.WriteLine($"Digite a nota do {i + 1}° aluno: ");
                 vetNotas[i] = float.Parse(Console.ReadLine());
             }
             return vetNotas;
+        }
+
+        static float media(float nota1, float nota2)
+        {
+            return (nota1 + nota2) / 2;
+        }
+
+        static void imprimir(float[] vetNotas)
+        {
+            for (int i = 0; i < vetNotas.Length; i++)
+            {
+                Console.Write($" {vetNotas[i]} |");
+            }
+            Console.WriteLine();
+        }
+
+        static void impMedia(float[] notPri, float[] notSeg)
+        {
+            Console.WriteLine("Media dos alunos da turma");
+            for (int i = 0; i < notPri.Length; i++)
+            {
+                Console.Write($"{media(notPri[i], notSeg[i])} |");
+            }
+        }
+
+        static void impResul(float[] notPri, float[] notSeg)
+        {
+            for (int i = 0; i < notPri.Length; i++)
+            {
+                if ( media(notPri[i],notSeg[i]) >= 6)
+                {
+                    Console.WriteLine($"O aluno {i+1} está aprovado");
+                }
+                else
+                {
+                     Console.WriteLine($"O aluno {i+1} está reprovado");
+                }
+            }
         }
     }
 }
