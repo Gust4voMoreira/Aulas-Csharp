fc5ef3d79d0c0ed67643a52ef84b567467c1a68f
Gustavo Moreira <gm1230133@gmail.com>
2021-05-25 22:51:28 -0300

    14.2

---
+++ aula14.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace aula14._2
+{
+    class Program
+    {
+        /*
+            1) Faça um programa que tenha as seguintes funções e procedimentos, ou seja, método com retorno e sem retorno.   
+            * Uma função (método com retorno) que preencha um vetor de números inteiros cujo número de posições será informado pelo usuário;   
+            * Uma função que retorne a somatória dos números do vetor;   
+            * Uma função que retorne o maior número do vetor;   
+            * Uma função que retorne o menor número do vetor;   
+            * Uma função que tetorne o total de números pares do vetor;   
+            * Uma função que retorne o total de números impares do vetor;   
+            * Um procedimento (método void) que imprima o valor inteiro que irá receber como parâmetro, ou seja, um inteiro.	 
+            Obs: Esse método será utilizado para imprimir todos retornos das funções;
+            */
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
