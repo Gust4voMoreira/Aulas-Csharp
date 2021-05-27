1ca253dd083f1f057018196f6d57d9356b185620
Gustavo Moreira <gm1230133@gmail.com>
2021-05-27 10:53:36 -0300

    aula14.2

--- aula14.2/Program.cs
+++ aula14.2/Program.cs
@@ -4,7 +4,10 @@ namespace aula14._2
 {
     class Program
     {
-        /*
+        static void Main(string[] args)
+        {
+            //Data: 25/05/2021
+            /*
             1) Faça um programa que tenha as seguintes funções e procedimentos, ou seja, método com retorno e sem retorno.   
             * Uma função (método com retorno) que preencha um vetor de números inteiros cujo número de posições será informado pelo usuário;   
             * Uma função que retorne a somatória dos números do vetor;   
@@ -15,9 +18,46 @@ namespace aula14._2
             * Um procedimento (método void) que imprima o valor inteiro que irá receber como parâmetro, ou seja, um inteiro.	 
             Obs: Esse método será utilizado para imprimir todos retornos das funções;
             */
-        static void Main(string[] args)
+
+            int qtd;
+
+            Console.WriteLine("Digite a quatidade de números do vetor: ");
+            qtd = int.Parse(Console.ReadLine()); 
+            Console.WriteLine("Números do vetor:");
+            var qtdN = pVetor(qtd);
+            imprimir(qtdN);
+        }
+
+        static int[] pVetor(int qtd)
         {
-            
+            int[] vetQtd = new int[qtd];
+
+            for (int i = 0; i < vetQtd.Length; i++)
+            {
+                Console.WriteLine($"Digite o {i + 1}° número: ");
+                vetQtd[i] = int.Parse(Console.ReadLine());
+            }
+            return vetQtd;
+        }
+
+        static int[] vetSoma(int soma, int qtd)
+        {
+            int[] vetQtd = new int[qtd];
+
+            for (int i = 0; i < vetQtd.Length; i++)
+            {
+                
+            }
+            return vetQtd;
+        }
+
+        static void imprimir(int[] qtdN)
+        {
+            for (int i = 0; i < qtdN.Length; i++)
+            {
+                Console.Write($" {qtdN[i]} |");
+            }
+            Console.WriteLine();
         }
     }
 }
