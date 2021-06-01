2037a9e03a5543812f77307968106816f514887c
Gustavo Moreira <gm1230133@gmail.com>
2021-06-01 20:43:27 -0300

    aula15.2

--- aula15.2/Program.cs
+++ aula15.2/Program.cs
@@ -4,6 +4,12 @@ namespace aula15._2
 {
     class Program
     {
+        //Data: 29/05/2021 // 01/06/2021
+        /*
+        3) Faça um programa em c# que tenha uma função que calcule e retorne a distância
+        entre dois pontos ( x1, y1) e (x2, y2). Todos os números e valores de retorno devem ser 
+        do tipo float
+        */
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
