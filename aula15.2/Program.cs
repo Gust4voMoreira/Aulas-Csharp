78affbdf3a0a854f507a2a5daa957729a9bd6d04
Gustavo Moreira <gm1230133@gmail.com>
2021-06-01 20:41:28 -0300

    alteração

---
+++ aula15.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aula15._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- aula15/Program.cs
+++ aula15/Program.cs
@@ -5,26 +5,39 @@ namespace aula15
     class Program
     {
         //Data: 29/05/2021 // 01/06/2021
-        /*1) Faça um programa em c# que tenha uma função que receba dois números inteiros 
+        /*
+        1) Faça um programa em c# que tenha uma função que receba dois números inteiros 
         retorne o menor número.
+
+        2) Faça um programa em c# que tenha uma função que receba dois números inteiros 
+        retorne o maior número.
         */
         static void Main(string[] args)
         {
-            int num1, num2;
+            int menor, maior, num1, num2;
 
             Console.WriteLine("digite: ");
             num1 = int.Parse(Console.ReadLine());
             Console.WriteLine("digite: ");
             num2 = int.Parse(Console.ReadLine());
 
-            int menor = retMenor(num1, num2);
+            menor = retMenor(num1, num2);
+            maior = retMaior(num1, num2);
 
-            Console.WriteLine(menor);
+            Console.WriteLine($"Maior: {maior}, Menor: {menor}");
         }
 
         static int retMenor(int num1, int num2)
         {
             if (num1 < num2)
+                return num1;
+            else
+                return num2;
+        }
+
+        static int retMaior(int num1, int num2)
+        {
+            if (num1 > num2)
                 return num1;
             else
                 return num2;
