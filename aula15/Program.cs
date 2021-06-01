76d88ba801e4ef10bdaa3e9598365d71e4c8443c
Gustavo Moreira <gm1230133@gmail.com>
2021-06-01 20:01:28 -0300

    alteração

---
+++ aula15/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace aula15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Data: 29/05/2021
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
