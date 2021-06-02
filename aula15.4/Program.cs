78156e5955cf34f8c07d69628a90c006a79c6678
Gustavo Moreira <gm1230133@gmail.com>
2021-06-01 22:08:43 -0300

    alteração

---
+++ aula15.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace aula15._4
+{
+    class Program
+    {
+        //Data: 29/05/2021 // 01/06/2021
+        /*
+        5) Faça um programa em c# que leia 5 números inteiros positivos (utilize uma função que 
+        leia esse número e verifique se ele é positivo). Para cada número informado escrever a 
+        soma de seus divisores (exceto ele mesmo). 
+        Utilize a função SomaDivisores para obter a soma.
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
