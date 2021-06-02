564556e049c45420686d36fed479b00747572c20
Gustavo Moreira <gm1230133@gmail.com>
2021-06-01 21:19:33 -0300

    aula15.2

--- aula15.2/Program.cs
+++ aula15.2/Program.cs
@@ -8,24 +8,35 @@ namespace aula15._2
         /*
         3) Faça um programa em c# que tenha uma função que calcule e retorne a distância
         entre dois pontos ( x1, y1) e (x2, y2). Todos os números e valores de retorno devem ser 
-        do tipo float
+        do tipo float.
         */
         static void Main(string[] args)
         {
-            float x1, x2, y1, y2;
+            float x1, x2, y1, y2, distancia;
 
-            Console.WriteLine("digite: ");
-            num1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("digite: ");
-            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("digite o x1: ");
+            x1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("digite o x2: ");
+            x2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("digite o y1: ");
+            y1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("digite o y2: ");
+            y2 = float.Parse(Console.ReadLine());
 
-            menor = retMenor(num1, num2);
-            maior = retMaior(num1, num2);
+            distancia = calcDist(x1, x2, y1, y2);
+
+            Console.WriteLine($"Distância: {distancia}");
         }
 
         static float calcDist(float x1, float y1, float x2, float y2)
         {
-            return 0;
+            float a, b, c, d;
+            a = x2 - x1;
+            b = y2 - y1;
+            c = (a * a) + (b * b);
+            d = (float)Math.Sqrt(c);
+
+            return d;
         }
     }
 }
