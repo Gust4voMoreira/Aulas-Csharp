4f89dfad411c185778ab6dea5d09ba54148b68da
Gustavo Moreira <gm1230133@gmail.com>
2021-06-15 20:36:06 -0300

    aula16

--- aula16/Program.cs
+++ aula16/Program.cs
@@ -7,7 +7,30 @@ namespace aula16
         static void Main(string[] args)
         {
             //Data: 15/06/2021
-            Console.WriteLine("Hello World!");
+            var vetNumeros = PreencherVetor(5);
+
+            Imprimir(vetNumeros);
+        }
+
+        static int[] PreencherVetor(int qtd)
+        {
+            var vet = new int[qtd];
+
+            for (int i = 0; i < vet.Length; i++)
+
+            {
+                vet[i] = int.Parse(Console.ReadLine());
+
+            }
+            return vet;
+        }
+
+        static void Imprimir(int[] vet)
+        {
+            for (int i = 0; i < vet.Length; i++)
+            {
+                Console.Write($"| {vet[i]} ");
+            }
         }
     }
 }
