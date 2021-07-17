1080f35ce61d61c42d31b853cafdf90af6fc33b7
Gustavo Moreira <gm1230133@gmail.com>
2021-07-17 11:05:23 -0300

    ex

--- ex/Program.cs
+++ ex/Program.cs
@@ -4,58 +4,43 @@ namespace ex
 {
     class Program
     {
+        /*2)Faça um programa que tenha uma função que preenchera um vetor cujo tamanho seja informado 
+            pelo usuário. O programa deverá solicitar o preenchimento de dois vetores. O programa deverá ter 
+            um procedimento (método do tipo void) que irá imprimir o produto dos valores dos dois vetores.*/
         static void Main(string[] args)
         {
-            int[] vetNum = new int[5];
+            Console.WriteLine("Digite o tamanho dos vetores:");
+            int tamanho = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < vetNum.Length; i++)
+            Console.WriteLine("//////////////////////");
+            var imprimir = preVet1(tamanho);
+            
+        }
+
+        static int[] preVet1(int tamanho)
+        {
+            int[] vetNum = new int[tamanho];
+
+            for (int i = 0; i < tamanho; i++)
             {
-                Console.Write($"Digite o {i + 1}º Numéro: ");
+                Console.WriteLine($"Digite o {i+1}° número do primeiro vetor:");
                 vetNum[i] = int.Parse(Console.ReadLine());
-
-                if (vetNum[i] <= 0)
-                {
-                    Console.Write("\nDigite numeros maiores que 0, Pressione Para Continuar");
-                    Console.ReadKey();
-                    Console.Clear();
-
-                    i = i - 1;
-                }
             }
-            var maNum = MaioNum(vetNum);
-            var media = MediaNum(vetNum);
-        }
-
-        static int[] MaioNum(int[] vetNum)
-        {
-            int maior = 0;
-            for (int i = 0; i < vetNum.Length; i++)
-            {
-
-                if (vetNum[i] > maior)
-                {
-                    maior = vetNum[i];
-                }
-            }
-            Console.Write($"\nO maior número digitado é: {maior} ");
-
             return vetNum;
         }
 
-        static int[] MediaNum(int[] vetNum)
+        static int[] preVet2(int tamanho)
         {
-            float media = 0, soma = 0, cont = 0;
-            for (int i = 0; i < vetNum.Length; i++)
+            int[] vetNum = new int[tamanho];
+
+            for (int i = 0; i < tamanho; i++)
             {
-                soma = (float)soma + vetNum[i];
-                cont++;
+                Console.WriteLine($"Digite o {i+1}° número do segundo vetor:");
+                vetNum[i] = int.Parse(Console.ReadLine());
             }
-
-            media = (float)soma / cont;
-
-            Console.Write($"\nA média dos números digitados é: {media} ");
-
             return vetNum;
         }
+
+        
     }
 }
