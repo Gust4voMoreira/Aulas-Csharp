e2a537808c7ce679eb17b340723cb74ca7b2232d
Gustavo Moreira <gm1230133@gmail.com>
2021-08-11 14:23:56 -0300

    alteração

--- ex1_Bacalhau/Program.cs
+++ ex1_Bacalhau/Program.cs
@@ -5,7 +5,7 @@ namespace ex1_Bacalhau
     class Program
     {
         /*
-        Desenvolva um programa capaz de ler um valor inteiro N. N * 2 linhas de saída
+        1) Desenvolva um programa capaz de ler um valor inteiro N. N * 2 linhas de saída
         vão ser apresentadas na execução do programa, seguindo a lógica do exemplo
         mais abaixo. Para os valores com mais de seis dígitos, todos os dígitos devem
         ser apresentados.
--- ex2_Bacalhau/Program.cs
+++ ex2_Bacalhau/Program.cs
@@ -4,60 +4,26 @@ namespace ex2_Bacalhau
 {
     class Program
     {
+        /*
+        2) Leia 3 valores, no caso, variáveis A, B e C, que são as três notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 2, a nota B tem peso 3 e a nota C tem peso 5. Considere que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
+        obs: Imprima a variável MEDIA conforme exemplo abaixo, com 1 dígito após o ponto decimal e com um espaço em branco antes e depois da igualdade. 
+
+        */
         static void Main(string[] args)
         {
-            int tamanho = 10;
+            Console.WriteLine($"Digite a nota A do aluno:");
+            double notaA = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"Digite a nota B do aluno:");
+            double notaB = double.Parse(Console.ReadLine());
+     
+            Console.WriteLine($"Digite a nota C do aluno:");
+            double notaC = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("//////////PARCIAL////////////");
-            var vet1 = vetUm(tamanho);
-            Console.WriteLine("//////////OFICIAL////////////");
-            var vet2 = vetDois(tamanho);
             Console.WriteLine("//////////MÉDIA////////////");
-            var vetM = vetMedia(tamanho, vet1, vet2);
-            imprimir(vetM, vet1, vet2);
-        }
+            double media = ((notaA * 2) + (notaB * 3) + (notaC * 5)) / 100;
 
-        static float[] vetUm(int tamanho)
-        {
-            float[] vetNum = new float[tamanho];
-
-            for (int i = 0; i < tamanho; i++)
-            {
-                Console.WriteLine($"Digite a nota parcial do {i+1}° aluno:");
-                vetNum[i] = float.Parse(Console.ReadLine());
-            }
-            return vetNum;
-        }
-
-        static float[] vetDois(int tamanho)
-        {
-            float[] vetNum = new float[tamanho];
-
-            for (int i = 0; i < tamanho; i++)
-            {
-                Console.WriteLine($"Digite a nota oficial do {i+1}° aluno:");
-                vetNum[i] = float.Parse(Console.ReadLine());
-            }
-            return vetNum;
-        }
-
-        static float[] vetMedia(int tamanho, float[] vet1, float[] vet2)
-        {
-            float[] vetNum = new float[tamanho];
-
-            for (int i = 0; i < tamanho; i++)
-            {
-                vetNum[i] = ((vet1[i] * 2) + (vet2[i] * 8)) / 100;
-            }
-            return vetNum;
-        }
-
-        static void imprimir(float[] vetM, float[] vet1, float[] vet2)
-        {
-            for (int i = 0; i < vet1.Length; i++)
-            {
-                Console.WriteLine($"Notas do {i+1}° aluno: \nPARCIAL: {vet1[i]/10}\nOFICIAL: {vet2[i]/10}\nMÉDIA: {vetM[i].ToString("N1")}");
-            }
+            Console.WriteLine($"Notas do aluno: \nA: {notaA/10}\nB: {notaB/10}\nC: {notaC/10}\nMÉDIA: {media.ToString("N1")}");
         }
     }
 }
