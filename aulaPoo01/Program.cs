15986facd6e5238c99d786ce98c289ea94d2825b
Gustavo Moreira <gm1230133@gmail.com>
2021-08-18 13:37:10 -0300

    alteração

---
+++ aulaPoo01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace aulaPoo01
+{
+    class Program
+    {
+        /*Valkiria é a diretora da escola Mundo Melhor .
+        Ela precisa de um programa que armazene alunos com suas matrículas e notas em uma "tabela" no 
+        final,ela tem a necessidade de saber se o aluno foi aprovado ou não faça um programa para resolver a questão da Valkiria*/
+        static void Main(string[] args)
+        {
+            //obs: eu não sei quantas notas são, nem quantos alunos.
+            int qtd;
+            float media, nota1, nota2;
+
+            console.writeLine("Digite a quantidade de alunos: ");
+            qtd = int.parse(console.rea);
+
+            for (int i = 0; i < length; i++)
+            {
+                
+            }
+        }
+    }
+}
