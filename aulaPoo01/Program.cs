a7d8bebf9ab13765dd3da362c4681e0dd4bdfee2
Gustavo Moreira <gm1230133@gmail.com>
2021-08-25 14:49:50 -0300

    alteração

--- aulaPoo01/Program.cs
+++ aulaPoo01/Program.cs
@@ -8,37 +8,62 @@ namespace aulaPoo01
         static void Main(string[] args)
         {
             //obs: eu não sei quantas notas são, nem quantos alunos, nem a media :(
-            int qtd;
-            double n1, n2;
+            // int qtd;
+            // double n1, n2;
 
-            Console.WriteLine("Digite o número de alunos:");
-            qtd = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Bem-vindo, Walkiria!");
+            // Console.WriteLine("Digite o número de alunos:");
+            // qtd = int.Parse(Console.ReadLine());
 
-            double[] vetMedia = new double[qtd];
+            // double[] vetMedia = new double[qtd];
 
-            for (int i = 0; i < qtd; i++)
-            {
-                Console.WriteLine($"Digite a nota do Primeiro Bimestre do {i+1}° aluno: ");
-                n1 = double.Parse(Console.ReadLine());
-                Console.WriteLine($"Digite a nota do Segundo Bimestre do {i+1}° aluno: ");
-                n2 = double.Parse(Console.ReadLine());
-
-                vetMedia[i] = n1 + n2;
-            }
             
-            for (int i = 0; i < qtd; i++)
+            
+
+////////////////////////////
+        var nome = "";
+        int matricula = 0;
+        var opcao = "s";
+
+        Console.WriteLine("Bem-vindo, Walkiria!\n");
+
+        while (opcao.ToLower() =="s" )
+        {
+            
+            Console.WriteLine("Digite o nome do aluno:");
+            nome = Console.ReadLine();
+            Console.WriteLine("Digite a Matricula do aluno:");
+            matricula = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nDigite as Notas do aluno: \n");
+            double [] nota = new double[3];
+
+            double soma = 0;
+            double media = 0;
+        
+            for(int i=0; i<3; i++)
             {
-                Console.WriteLine($"Média do {i+1}° aluno foi {vetMedia[i]/2}");
-                if (vetMedia[i]/2 < 6)
-                {
-                    Console.WriteLine("Não Aprovado");
-                }
-                else
-                {
-                    Console.WriteLine("Aprovado");
-                }
-                
+                Console.WriteLine($"{i + 1}ª nota:");
+                nota[i] = Convert.ToDouble(Console.ReadLine());
+                soma += nota[i];
             }
+
+            media = soma / nota.Length;
+
+            Console.WriteLine($"\nMédia do Aluno {nome} foi de {media.ToString("N1")}");
+
+            if( media >= 6)
+            {
+                Console.WriteLine("APROVADO\n");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO\n");
+            }
+    
+            Console.WriteLine("Deseja cadastrar um novo aluno? S ou N: \n");
+            opcao = Console.ReadLine();
+            Console.Clear();
+        }
             
         }
     }
