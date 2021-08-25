8a949ea617786e840cb3b37633067f31c6d03e40
Gustavo Moreira <gm1230133@gmail.com>
2021-08-25 15:12:07 -0300

    Update Program.cs

--- aulaPoo01/Program.cs
+++ aulaPoo01/Program.cs
@@ -1,70 +1,55 @@
 ﻿using System;
 
 namespace aulaPoo01
-{
-    class Program
+{       
+    class Program   
     {
         /*Valkiria é a diretora da escola Mundo Melhor. Ela precisa de um programa que armazene alunos com suas matrículas e notas em uma "tabela" no final,ela tem a necessidade de saber se o aluno foi aprovado ou não faça um programa para resolver a questão da Valkiria*/
         static void Main(string[] args)
         {
-            //obs: eu não sei quantas notas são, nem quantos alunos, nem a media :(
-            // int qtd;
-            // double n1, n2;
+            var nome = "";
+            var opcao = "s";
+            int matricula = 0;
 
-            // Console.WriteLine("Bem-vindo, Walkiria!");
-            // Console.WriteLine("Digite o número de alunos:");
-            // qtd = int.Parse(Console.ReadLine());
+            Console.WriteLine("Bem-vindo, Walkiria!\n");
 
-            // double[] vetMedia = new double[qtd];
+            while (opcao.ToLower() =="s" )
+            {
+                
+                Console.WriteLine("Digite o nome do aluno:");
+                nome = Console.ReadLine();
+                Console.WriteLine("Digite a Matricula do aluno:");
+                matricula = int.Parse(Console.ReadLine());
+                Console.WriteLine("\nDigite as Notas do aluno: \n");
+                double [] nota = new double[3];
 
+                double soma = 0;
+                double media = 0;
             
-            
+                for(int i=0; i<3; i++)
+                {
+                    Console.WriteLine($"{i + 1}ª nota:");
+                    nota[i] = Convert.ToDouble(Console.ReadLine());
+                    soma += nota[i];
+                }
 
-////////////////////////////
-        var nome = "";
-        int matricula = 0;
-        var opcao = "s";
+                media = soma / nota.Length;
 
-        Console.WriteLine("Bem-vindo, Walkiria!\n");
+                Console.WriteLine($"\nMédia do Aluno {nome} foi de {media.ToString("N1")}");
 
-        while (opcao.ToLower() =="s" )
-        {
-            
-            Console.WriteLine("Digite o nome do aluno:");
-            nome = Console.ReadLine();
-            Console.WriteLine("Digite a Matricula do aluno:");
-            matricula = int.Parse(Console.ReadLine());
-            Console.WriteLine("\nDigite as Notas do aluno: \n");
-            double [] nota = new double[3];
-
-            double soma = 0;
-            double media = 0;
+                if( media >= 6)
+                {
+                    Console.WriteLine("APROVADO\n");
+                }
+                else
+                {
+                    Console.WriteLine("REPROVADO\n");
+                }
         
-            for(int i=0; i<3; i++)
-            {
-                Console.WriteLine($"{i + 1}ª nota:");
-                nota[i] = Convert.ToDouble(Console.ReadLine());
-                soma += nota[i];
-            }
-
-            media = soma / nota.Length;
-
-            Console.WriteLine($"\nMédia do Aluno {nome} foi de {media.ToString("N1")}");
-
-            if( media >= 6)
-            {
-                Console.WriteLine("APROVADO\n");
-            }
-            else
-            {
-                Console.WriteLine("REPROVADO\n");
-            }
-    
-            Console.WriteLine("Deseja cadastrar um novo aluno? S ou N: \n");
-            opcao = Console.ReadLine();
-            Console.Clear();
-        }
-            
+                Console.WriteLine("Deseja cadastrar um novo aluno? S ou N: \n");
+                opcao = Console.ReadLine();
+                Console.Clear();
+            }    
         }
     }
 }
