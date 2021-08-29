e17bfe05c3501582d38388100f4bd03dc34b3ed0
Gustavo Moreira <gm1230133@gmail.com>
2021-08-29 10:46:42 -0300

    alteração

--- aulaPoo01/Program.cs
+++ aulaPoo01/Program.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-namespace Desafio
+
+namespace Desafio 
 {
-	class Program
+	class Program 
 	{
-		static void Main(string[] args)
+		static void Main(string[] args) 
 		{
 			Console.WriteLine("Bem-vindo, Walkiria!\n");
 			var nome = "";
 			int matricula = 0;
 			var resultado = "";
 			var opcao = "s";
-			List<dynamic> alunos = new List<dynamic>();
+			List < dynamic > alunos = new List < dynamic > ();
 
-			while (opcao.ToLower() == "s")
+			while (opcao.ToLower() == "s") 
 			{
 				Console.WriteLine("Digite nome do aluno: \n");
 				nome = Console.ReadLine();
@@ -24,48 +25,57 @@ namespace Desafio
 				double soma = 0;
 				double media = 0;
 
-				for (int i = 0; i < 3; i++)
+				for (int i = 0; i < 3; i++) 
 				{
 					Console.WriteLine($"{i + 1}ª nota:");
 					notas[i] = Convert.ToDouble(Console.ReadLine());
 					soma += notas[i];
 				}
+
 				media = soma / notas.Length;
-				if (media >= 6)
+
+				if (media >= 6) 
 				{
 					resultado = "Aprovado";
-				}
-				else
+				} 
+				else 
 				{
 					resultado = "Reprovado";
 				}
-				alunos.Add(new
+
+				alunos.Add(new 
 				{
 					Nome = nome,
 					Matricula = matricula,
 					Notas = notas,
 					Media = media,
 					Situacao = resultado
-				});
+				}
+				);
+
 				Console.Clear();
 				Console.WriteLine("Deseja cadastrar um novo aluno? S ou N: \n");
 				opcao = Console.ReadLine();
 			}
+
 			Console.Clear();
-			foreach(var aluno in alunos)
+
+			foreach(var aluno in alunos) 
 			{
-				Console.WriteLine($"--------------------------------------");
+				Console.WriteLine("--------------------------------------");
 				Console.WriteLine($"Nome: {aluno.Nome} - Matricula : {aluno.Matricula}");
-				Console.WriteLine($"--------------------------------------");
-				Console.WriteLine($"Notas: ");
+				Console.WriteLine("--------------------------------------");
+				Console.WriteLine("Notas: ");
 				int n = 1;
-				foreach (double nota in aluno.Notas)
+
+				foreach(double nota in aluno.Notas) 
 				{
 					Console.WriteLine($"{n}ª Nota :{nota}");
 					n += 1;
 				}
+
 				Console.WriteLine($"Média: {Math.Round(aluno.Media,2)} - Situação: {aluno.Situacao}");
-				Console.WriteLine($"--------------------------------------");
+				Console.WriteLine("--------------------------------------");
 			}
 		}
 	}
