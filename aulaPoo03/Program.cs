f527dabf9da06c702c59924b09c5fc153ce3a10f
Gustavo Moreira <gm1230133@gmail.com>
2021-09-28 20:45:10 -0300

    aulaPoo03

---
+++ aulaPoo03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace aulaPoo03
+{
+    class Program
+    {
+        //DESAFIO 06
+        /*
+        Desenvolver uma sistema de locadora onde calcula o seguro pago do cliente , obedecendo os seguintes critérios:
+        Valor do seguro:
+        Carro Popular 10%
+        Carro de Luxo 20%
+        Carro utilitário 25%
+
+        Valor das diárias :
+        Carro Popular R$ 100,00
+        Carro de Luxo R$ 300,00
+        Carro utilitário R$ 400,00
+
+        Obs: a porcentagem é em cima do valor utilizado
+        */
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
