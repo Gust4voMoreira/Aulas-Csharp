9892f7ecebfa37a8292ab0c74874cd8baba75a07
Gustavo Moreira <gm1230133@gmail.com>
2021-10-30 14:53:38 -0300

    Create Pokedex.cs

---
+++ aulaPoo04/Pokedex.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aulaPoo04
+{
+    public class Pokedex
+    {
+        private List<Pokemon> pokemons;
+        public List<Pokemon> Pokemons
+        {
+            get { return pokemons; }
+        }
+
+        private void InicializaLista()
+        {
+
+        }
+        
+    }
+}
