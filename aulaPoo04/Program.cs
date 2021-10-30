c20aa903456b02b8c6dc5def08167f185edc6b27
Gustavo Moreira <gm1230133@gmail.com>
2021-10-30 14:40:22 -0300

    aulaPoo04

--- aulaPoo04/Program.cs
+++ aulaPoo04/Program.cs
@@ -8,11 +8,13 @@ namespace aulaPoo04
         {
             Pokemon p = new Pokemon("Bulbasauro", "Bulbasauro é um pokémon do tipo planta..");
             Pokemon p1 = new Pokemon();
+            
             Console.WriteLine("Digite o nome: ");
             p1.Nome = Console.ReadLine();
             Console.WriteLine("Digite a descrição: ");
             p1.Descricao = Console.ReadLine();
             Console.Clear();
+
             p.ExibirDadosPokemon();
             Console.WriteLine("///////////////////////");
             p1.ExibirDadosPokemon();
