86f901cfd5a5d0ebc5a8c0b38a4523e37db3301d
Gustavo Moreira <gm1230133@gmail.com>
2021-11-06 10:42:23 -0300

    Update Program.cs

---
+++ aulaPoo04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aulaPoo04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pokedex pokedex = new Pokedex();
+            pokedex.ListarPokemons();
+            Console.ReadKey();
+        }
+    }
+}
