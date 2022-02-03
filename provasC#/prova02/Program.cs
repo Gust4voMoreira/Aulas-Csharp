5ac1f48a5ce90d71fb490ad26844ffe096134959
Gustavo Moreira <gm1230133@gmail.com>
2022-02-03 17:38:15 -0300

    alteração

---
+++ provasC#/prova02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace prova02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           newclass pessoa = new newclass();
+           //pessoa.Nome = Console.ReadLine();
+           //Console.WriteLine(pessoa.Nome);
+        }
+    }
+}
---
+++ provasC#/prova02/newclass.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prova02
+{
+    public class newclass
+    {
+        public void Nome(string nomePessoa){
+            this.nomePessoa = nomePessoa;
+        }
+    }
+}
